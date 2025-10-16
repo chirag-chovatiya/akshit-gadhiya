2bd0692cf60d3cc4d15de1e93c9a146f769efaeb
chirag-chovatiya <134358879+chirag-chovatiya@users.noreply.github.com>
2025-10-16 08:53:37 +0530

    fix(preloader): Update background color to match theme

--- src/components/loader/Preloader.jsx
+++ src/components/loader/Preloader.jsx
@@ -18,7 +18,7 @@ export default function Preloader() {
   if (!showLoader) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center blue z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#0f172a] z-50">
       <div className="relative w-16 h-16">
         <div className="absolute w-full h-full border-4 border-t-custom-orange border-b-custom-orange border-l-transparent border-r-transparent rounded-full animate-spin shadow-lg"></div>
         <div className="absolute w-8 h-8 bg-custom-orange rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-pulse shadow-md"></div>
