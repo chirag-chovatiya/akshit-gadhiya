ef225796c1a3da1b1797e84f93fc62b2b4635df1
chirag <chiragchovatiya92@gmail.com>
2026-03-21 22:30:43 +0530

    Implement blog details page functionality and enhance SEO with robots.txt, sitemap.xml, and Google site verification.

--- src/service/blog-api.js
+++ src/service/blog-api.js
@@ -1,9 +1,12 @@
 import { get } from "./api";
 import { API } from "./constant/api-constant";
 
-export async function getAllBlog () {
+export async function getAllBlog() {
   return await get(API.blog);
-};
-export async function getAllCategory () {
+}
+export async function getBlogById(id) {
+  return await get(`${API.blog}/${id}`);
+}
+export async function getAllCategory() {
   return await get(API.blogcategory);
-};
+}
